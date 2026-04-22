25362713363803e35cdc3d5caccb79a025151dc2
TheFoxerThatFoxed <214658836+UnFoxable@users.noreply.github.com>
2026-04-23 03:11:01 +0800

    Add files via upload

--- 9/Javascript9.js
+++ 9/Javascript9.js
@@ -6,67 +6,143 @@ var dctx = dcanvas.getContext('2d');
 rctx.imageSmoothingEnabled = true;
 resize();
 
+
 function draw() {
     color = document.querySelector('#pickcolour').value;
 
-    rctx.lineWidth = 5;
+    inptwidth = document.querySelector('#inptwidth').value;
+    if (!inptwidth) {
+        inptwidth = 3;
+    }
+
+    rctx.lineWidth = inptwidth;
+    rctx.lineCap = 'round';
+    rctx.lineJoin = 'round';
+    rctx.miterLimit = 1;
     rctx.strokeStyle = color;
     rctx.fillStyle = color;
 
-    rctx.lineTo(mouseX, mouseY);
-    rctx.stroke();
-    rctx.closePath();
-    display();
     rctx.beginPath();
-    rctx.moveTo(mouseX, mouseY);
+
+    cQ = (smooth+1)/4*2-1;
+    cR = (smooth+1)/4*3-1;
+
+    //Chaikin's Algorithm + BezierCurve (Smoothing Algorithm)
+    mouseX[cQ] = (mouseX[0] * (3/4)) + (mouseX[smooth] * (1/4))
+    mouseX[cR] = (mouseX[0] * (1/4)) + (mouseX[smooth] * (3/4))
+    mouseY[cQ] = (mouseY[0] * (3/4)) + (mouseY[smooth] * (1/4))
+    mouseY[cR] = (mouseY[0] * (1/4)) + (mouseY[smooth] * (3/4))
+
+    rctx.moveTo(mouseX[0], mouseY[0]);
+    rctx.bezierCurveTo(mouseX[cQ], mouseY[cQ], mouseX[cR], mouseY[cR], mouseX[smooth], mouseY[smooth]);
+
+    // console.log(mouseX);
+    // console.log(mouseY);
+
+    rctx.closePath();
+
+    rctx.stroke();
+    display();
 }
 
 // copy from rcanvas to dcanvas
-var displaystore;
+// var displaystore;
 function display() {
-    displaystore = rctx.getImageData(0, 0, rcanvas.clientWidth, rcanvas.clientHeight);
-    dctx.putImageData(displaystore, 0, 0);
+    // displaystore = rctx.getImageData(0, 0, rwidth, rheight);
+    // dctx.putImageData(displaystore, 0, 0);
+    var size = document.querySelector('#size').value;
+    console.log(size);
+    dctx.clearRect(0, 0, rwidth, rheight);
+    dctx.drawImage(rcanvas, rwidth, rheight, rwidth*size, rheight*size)
 }
 
+document.querySelector('#size').addEventListener('input', (e) => {
+    display();
+})
+
+// stamp different shapes
+function stamp(type, size) {
+    switch(true) {
+        case type == 'circle':
+            circle(size);
+            break;
+        default:
+            break;
+    }
+}
+
+function circle(size) {
+    color = document.querySelector('#pickcolour').value;
+
+    rctx.lineWidth = inptwidth;
+    rctx.strokeStyle = color;
+    rctx.fillStyle = color;
+
+    rctx.beginPath()
+
+    rctx.moveTo(mouseX[0], mouseY[0]);
+    rctx.arc( mouseX[0], mouseY[0], size/100, 0, 2*Math.PI);
+    rctx.fill()
+
+    rctx.closePath();
+    rctx.stroke();
+    display();
+}
+
+
 // mouse tracking
-var mouseX, mouseY, mouseState = 0;
+var smooth = 3*1;
+var mouseX = [smooth];
+var mouseY = [smooth];
+var mouseState = 0;
 window.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+    mouseX[smooth] = mouseX[0];
+    mouseY[smooth] = mouseY[0];
+
+    mouseX[0] = e.clientX;
+    mouseY[0] = e.clientY;
+
+    // for (var i=3;i>0;i--) {
+    //     mouseX[i] = mouseX[i-1];
+    //     mouseY[i] = mouseY[i-1];
+    // }
+    // console.log(mouseX);
+
     if(mouseState == 1) {
         draw();
     }
 })
 window.addEventListener('mousedown', (e) => {
-    rctx.moveTo(mouseX, mouseY);
+    inptwidth = document.querySelector('#inptwidth').value;
+    if (!inptwidth) {
+        inptwidth = 3;
+    }
+    stamp('circle', inptwidth);
     mouseState = 1;
 })
 window.addEventListener('mouseup', (e) => {
     mouseState = 0;
-    rctx.moveTo(mouseX, mouseY);
-    rctx.stroke();
-    rctx.closePath();
 })
 
 // resize
 var resizestore;
 window.addEventListener('resize', (e) => {
-    resizestore = rctx.getImageData(0, 0, cwidth, cheight);
+    resizestore = rctx.getImageData(0, 0, rwidth, rheight);
     resize();
     rctx.putImageData(resizestore, 0, 0);
     display();
 })
 function resize() {
-    cwidth = rcanvas.clientWidth;
-    cheight = rcanvas.clientHeight;
-    rcanvas.width = cwidth;
-    rcanvas.height = cheight;
-    dcanvas.width = cwidth;
-    dcanvas.height = cheight;
+    rwidth = rcanvas.clientWidth;
+    rheight = rcanvas.clientHeight;
+    rcanvas.width = rwidth;
+    rcanvas.height = rheight;
+    dcanvas.width = rwidth;
+    dcanvas.height = rheight;
 }
 
 // clear everything
 function trash() {
-    rctx.clearRect(0, 0, cwidth, cheight);
+    rctx.clearRect(0, 0, rwidth, rheight);
     display();
 }
